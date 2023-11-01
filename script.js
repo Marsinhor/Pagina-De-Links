bead3700d9a5bbb8c0805d1d847bc9884b946545
Marcio <you@example.com>
2023-11-01 15:11:02 -0300

    second commit

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ function toggleMode() {
 const body = document.body
 body.classList.toggle("light")
 
-
 const img = document.querySelector("#profile img")
   if(body.classList.contains('light')) {img.setAttribute('src', './assets/avatar-light.png')} 
   else {img.setAttribute('src', './assets/avatar.png')}
 
-const alt = document.querySelector("#profile img alt")  
-  if (body.classList.contains('light')) {alt.setAttribute('alt', 'Foto de Marcio Carvalho Light')} 
-  else {alt.setAttribute('alt', 'Foto de Marcio Carvalho Dark')}
+var profile = document.querySelector(".profile")
+  if(body.classList.contains('light')) {profile.alt = "Foto de Marcio Carvalho modo claro"} 
+  else {profile.alt = "Foto de Marcio Carvalho modo escuro"}
   
-  }
+}
